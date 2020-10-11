a1c110968a7a0402fbe439aadc2595dd84f34494
Tim Flucker <tflucker@gwmail.gwu.edu>
2020-10-11 17:10:49 -0400

    give winning squares yellow background

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 
 function Square(props) {
     return (
-        <button className="square" onClick={props.onClick}>
+        <button className={"square "+ (props.isWinning ? "square-winning" : null)} onClick={props.onClick}>
             {props.value}
         </button>
     );
@@ -15,6 +15,7 @@ class Board extends React.Component {
     renderSquare(i) {
         return (
             <Square
+                isWinning={this.props.winningSquares.includes(i)}
                 key={"square " + i} 
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
@@ -114,7 +115,7 @@ class Game extends React.Component {
 
         let status;
         if (winner) {
-            status = 'Winner: ' + winner;
+            status = 'Winner: ' + winner.player;
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
@@ -123,6 +124,7 @@ class Game extends React.Component {
             <div className="game">
                 <div className="game-board">
                     <Board
+                        winningSquares={winner ? winner.line : []}
                         squares={current.squares}
                         onClick={(i) => this.handleClick(i)}
                     />
@@ -151,7 +153,7 @@ function calculateWinner(squares) {
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+            return {player: squares[a], line: [a,b,c]};
         }
     }
     return null;
